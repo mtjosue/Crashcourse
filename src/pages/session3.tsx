a682458cdbd93d2061c15cd9f0816d9c2fa26bf1
Josue Mata <mtjosue@gmail.com>
2024-04-25 10:13:54 -0400

    -Updates of session 4

---
+++ src/pages/session3.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const session3 = () => {
+  const a = {
+    name: "Josue",
+  };
+  const b = [1, 2, 3];
+
+  const hongosDeLarry = {
+    set: 1,
+    setting: 2,
+    hongos: 3,
+  };
+
+  // navigator.mediaDevices.
+
+  // console.log("Hongos LOGEADOOS", console);
+
+  // console.
+
+  // console.log("Now");
+
+  // function exampleFunction() {
+  //   return "help";
+  // }
+
+  const exampleArray = ["Carlos", "Ana", "Pedro"];
+
+  console.log("Who am I logging", exampleArray[2]);
+
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-500">
+      <span className="text-white">This is the example page for testing</span>
+    </div>
+  );
+};
+
+export default session3;
