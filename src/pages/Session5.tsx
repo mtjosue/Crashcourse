4d527f43aac2dce1e7bf7adfbe047b19cb217265
Josue Mata <mtjosue@gmail.com>
2024-05-03 13:15:54 -0400

    Adding new sessions and updating the default package manager for this project.

--- src/pages/Session5.tsx
+++ src/pages/Session5.tsx
@@ -1,6 +1,25 @@
 // props: Props
 const Session5 = () => {
   //
+  const cosechas = {
+    marzo: 4,
+    abril: 7,
+    mayo: 9,
+  };
+  //console.log("-----------------------------------------");
+  //console.log(" objectproperty-abril", cosechas.abril);
+  const loSe = "marzo";
+  //console.log(" objectproperty-abril", cosechas[loSe]);
+
+  for (let i = 0; i < 5; i++) {
+    const ran = Math.random() * 100;
+    const ran2 = Math.floor(ran);
+    console.log("ran2", ran2);
+  }
+
+  const diasDeSemana = ["lunes", "martes"];
+  // diasDeSemana.
+  Object.keys(cosechas);
   return <div>Session5</div>;
 };
 
