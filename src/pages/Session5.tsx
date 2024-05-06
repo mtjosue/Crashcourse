aab6c8d96df46931a99c073a0a50201a9b042a79
Josue Mata <mtjosue@gmail.com>
2024-05-06 16:36:49 -0400

    Updates to notes

--- src/pages/Session5.tsx
+++ src/pages/Session5.tsx
@@ -1,26 +1,61 @@
+import { useEffect } from "react";
+
 // props: Props
 const Session5 = () => {
   //
-  const cosechas = {
-    marzo: 4,
-    abril: 7,
-    mayo: 9,
-  };
-  //console.log("-----------------------------------------");
-  //console.log(" objectproperty-abril", cosechas.abril);
-  const loSe = "marzo";
-  //console.log(" objectproperty-abril", cosechas[loSe]);
+  // const cosechas = {
+  //   marzo: 4,
+  //   abril: 7,
+  //   mayo: 9,
+  //   dummyFunc: (mensajito: () => void) => {
 
-  for (let i = 0; i < 5; i++) {
-    const ran = Math.random() * 100;
-    const ran2 = Math.floor(ran);
-    console.log("ran2", ran2);
+  //     // console.log("dd", mensajito.concat(" ANAAAAAA"))
+
+  //   }
+  // };
+
+  // // cosechas.dummyFunc("Firez")
+  // //console.log("-----------------------------------------");
+  // //console.log(" objectproperty-abril", cosechas.abril);
+  // const loSe = "marzo";
+  // //console.log(" objectproperty-abril", cosechas[loSe]);
+
+  // for (let i = 0; i < 5; i++) {
+  //   const ran = Math.random() * 100;
+  //   const ran2 = Math.floor(ran);
+  //   // console.log("ran2", ran2);
+  // }
+
+  // const diasDeSemana = ["lunes", "martes", "muchos", "muchos", "muchos", "muchos", "muchos", "muchos", "muchos", "Demasiado"];
+  // console.log("STRING TO DENOTE WHAT", diasDeSemana[diasDeSemana.length - 1])
+  // // diasDeSemana.
+  // // ()
+  // Object.keys(cosechas)
+  // // {}
+  // // diasDeSemana.
+  // Object.keys(cosechas);
+
+  // // diasDeSemana.
+  // // dia
+  // console.log(diasDeSemana.length)
+  // console.log("cosechas", typeof cosechas);
+  // console.log("diasDeSemana", typeof diasDeSemana);
+
+  
+  const house = 1;
+
+  const caller = () => {
+    setTimeout(() => {
+      console.log(house)
+      console.log("esperameeeeeee")
+    }, 5000)
   }
 
-  const diasDeSemana = ["lunes", "martes"];
-  // diasDeSemana.
-  Object.keys(cosechas);
-  return <div>Session5</div>;
+  caller()
+
+// useEffect(() => {}, [age, name])
+
+  
 };
 
 export default Session5;
