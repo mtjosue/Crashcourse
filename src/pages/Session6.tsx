f513938bbaac08b25e73bef459acae6a415c37ac
Josue Mata <mtjosue@gmail.com>
2024-05-08 22:10:45 -0400

    05-08-24

---
+++ src/pages/Session6.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Session6 = () => {
+  // setTimeout()
+  //   const test = ["a", "b", "c"];
+  //   const a = test[0]
+  return <div>Session6</div>;
+};
+
+export default Session6;
