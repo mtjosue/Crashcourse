83ceaad07265d4f9dddc293bb33d23c0528cd385
Josue Mata <mtjosue@gmail.com>
2024-05-09 11:27:55 -0400

    Adding Pseudo and syntax documents

--- src/pages/Session6.tsx
+++ src/pages/Session6.tsx
@@ -1,9 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Session6 = () => {
   // setTimeout()
-  //   const test = ["a", "b", "c"];
-  //   const a = test[0]
+
+  const a = 1;
+  const b = a.toString();
+  console.log(typeof b);
+  // => "1"
+  parseInt(b);
+  // => 1
+
+  const nestedObject = {
+    favoriteShows: [
+      "Game of Thrones",
+      "The Boys",
+      "Sex and the City",
+      "The Witcher",
+    ],
+    favoriteActivities: [
+      {
+        season: "Summer",
+        sport: "Volley Ball",
+      },
+      {
+        season: "Winter",
+        sport: "Snowboard",
+      },
+    ],
+  };
+
+  const c = "3.145674";
+  parseFloat(c);
+
+  const nestedArray = [
+    {
+      brand: "Toyota",
+      models: ["Camry", "Highlander", "Corolla"],
+    },
+    {
+      brand: "Honda",
+      models: ["Civic", "Accord", "Corolla"],
+    },
+  ];
+
+  const [score, setScore] = useState();
+
+  const test = {
+    first: "airplane",
+    second: "bus",
+    third: "car",
+    fourth: "door",
+    fifth: "escape",
+  };
+
+  //Destructuring
+  //   const fifth = test.fifth;
+  const { fifth } = test;
+
+  //Renaming & Default Values
+  const { fifth: newVar = true } = test;
+
+  //Value of a Arrow Function Expression?
+  const testFunc = () => {
+    return;
+  };
+
   return <div>Session6</div>;
 };
 
